d7209697ecd08f276d7044dba6ca2d1b71ae2ae3
Md Mostafizur Rahman <mostafiz67.cu.cse@gmail.com>
2022-08-16 12:06:03 -0300

    Add files via upload

---
+++ car_dealership_etl.py
@@ -0,0 +1,103 @@
+
+# Import the required modules and functions
+
+
+import glob                         # this module helps in selecting files 
+import pandas as pd                 # this module helps in processing CSV files
+import xml.etree.ElementTree as ET  # this module helps in processing XML files.
+from datetime import datetime
+
+# Download Files
+
+!wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0221EN-SkillsNetwork/labs/module%206/Lab%20-%20Extract%20Transform%20Load/data/datasource.zip
+
+# Unzip Files
+
+!unzip datasource.zip -d dealership_data
+
+# Set Paths
+
+tmpfile    = "temp.tmp"               # file used to store all extracted data
+logfile    = "logfile.txt"            # all event logs will be stored in this file
+targetfile = "transformed_data.csv"   # file where transformed data is stored
+
+# Extract
+
+# CSV Extract Function
+def extract_from_csv(file_to_process):
+    dataframe = pd.read_csv(file_to_process)
+    return dataframe
+
+# JSON Extract Function
+def extract_from_json(file_to_process):
+    dataframe = pd.read_json(file_to_process,lines=True)
+    return dataframe
+
+# XML Extract Function
+def extract_from_xml(file_to_process):
+    dataframe = pd.DataFrame(columns=["car_model", "year_of_manufacture", "price", "fuel"])
+    tree = ET.parse(file_to_process)
+    root = tree.getroot()
+    for car in root:
+        car_model = car.find("car_model").text
+        year_of_manufacture = car.find("year_of_manufacture").text
+        price = float(car.find("price").text)
+        fuel = car.find("fuel").text
+        dataframe = dataframe.append({"car_model":car_model, "year_of_manufacture":year_of_manufacture, "price":price, "fuel":fuel}, ignore_index=True)
+    return dataframe
+
+# Extract Function
+def extract():
+    extracted_data = pd.DataFrame(columns=['car_model','year_of_manufacture','price','fuel']) # create an empty data frame to hold extracted data
+    
+    #process all csv files
+    for csvfile in glob.glob("dealership_data/*.csv"):
+        extracted_data = extracted_data.append(extract_from_csv(csvfile), ignore_index=True)
+        
+    #process all json files
+    for jsonfile in glob.glob("dealership_data/*.json"):
+        extracted_data = extracted_data.append(extract_from_json(jsonfile), ignore_index=True)
+    
+    #process all xml files
+    for xmlfile in glob.glob("dealership_data/*.xml"):
+        extracted_data = extracted_data.append(extract_from_xml(xmlfile), ignore_index=True)
+        
+    return extracted_data
+
+# Transform
+def transform(data):
+        #Round the price columns to 2 decimal places
+        data['price'] = round(data.price, 2)
+
+        return data
+
+# Loading
+def load(targetfile,data_to_load):
+    data_to_load.to_csv(targetfile)
+
+# Logging
+def log(message):
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second
+    now = datetime.now() # get current timestamp
+    timestamp = now.strftime(timestamp_format)
+    with open("logfile.txt","a") as f:
+        f.write(timestamp + ',' + message + '\n')
+
+# Running ETL Process
+log("ETL Job Started")
+
+log("Extract phase Started")
+extracted_data = extract()
+log("Extract phase Ended")
+extracted_data
+
+log("Transform phase Started")
+transformed_data = transform(extracted_data)
+log("Transform phase Ended")
+transformed_data
+
+log("Load phase Started")
+load(targetfile,transformed_data)
+log("Load phase Ended")
+
+log("ETL Job Ended")
